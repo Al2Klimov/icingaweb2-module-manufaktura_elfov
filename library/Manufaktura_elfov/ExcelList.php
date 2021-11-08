b76dd717e32798d5915389c02f7dc0c502aa4256
Alexander A. Klimov <grandmaster@al2klimov.de>
2021-11-08 14:10:12 +0100

    ExcelList: support born dd.mm.YYYY

--- library/Manufaktura_elfov/ExcelList.php
+++ library/Manufaktura_elfov/ExcelList.php
@@ -91,7 +91,7 @@ EOF
                     $pp->source = $this->getUuid();
 
                     if ($bornIndex !== null && isset($row[$bornIndex])) {
-                        if (preg_match('~^(\d+ \w+ \d+)~', $row[$bornIndex], $matches)) {
+                        if (preg_match('~^(\d+ \w+ \d+|\d+\.\d+\.\d+)~', $row[$bornIndex], $matches)) {
                             $pp->born = new DateTime($matches[1]);
                         }
                     }
